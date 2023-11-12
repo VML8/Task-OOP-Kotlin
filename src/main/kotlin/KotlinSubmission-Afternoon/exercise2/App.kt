e188fbd40ee46d0f2cd4616e2c619f41714c1190
VML8 <92667459+VML8@users.noreply.github.com>
2023-11-12 16:46:12 +0700

    Exercise 2 Complete Used LocalDate for extra fanciness. uwu

--- src/main/kotlin/KotlinSubmission-Afternoon/exercise2/App.kt
+++ src/main/kotlin/KotlinSubmission-Afternoon/exercise2/App.kt
@@ -1,5 +1,7 @@
 package id.infinitelearning.KotlinSubmission.exercise2
 
+import java.time.LocalDate
+
 fun main() {
 
     /** Latihan 1 List
@@ -9,6 +11,13 @@ fun main() {
     - Agar lebih mudah, gunakanlah for loop dan logika if untuk mengisi bilangan genap pada list
      */
     // Buat di bawah sini
+    val evenNumber = mutableListOf<Int>()   // Declaration
+
+    for (i in 1..100) {
+        if (i %2 == 0) {                    // Even number check
+            evenNumber.add(i)
+        }
+    }
 
 
     /** Latihan 2 Map
@@ -17,19 +26,45 @@ fun main() {
     - Set Key nya dengan inisial nama bulan dan Value nya nama bulan, contoh
     - key "Jan", value January
     - key "Feb", value February
-    - dst...
+    - dst... */ // Close this pls thanks
     // Buat di bawah sini
 
+    val monthMap = mapOf(
+        "Jan" to "January",
+        "Feb" to "February",
+        "Mar" to "March",
+        "Apr" to "April",
+        "May" to "May",
+        "Jun" to "June",
+        "Jul" to "July",
+        "Aug" to "August",
+        "Sep" to "September",
+        "Oct" to "October",
+        "Nov" to "November",
+        "Dec" to "December"
+    )
+
+    /**
     Cetak semua nama bulan dengan fungsi forEach sehingga hasil output seperti:
     - Jan -> January
     - Feb -> February
-    - Dst...
+    - Dst... */ // Close this pls thanks
     // Buat di bawah sini
 
+    monthMap.forEach { (key, value) -> println("$key -> $value")
+    }
+
+    /**
     Cetak nama bulan sekarang dan bulan lahir kamu dengan format string berikut:
     - "It's {$monthNow} now, I was born in {$birthMonth}"
      */
     // Buat di bawah sini
 
+    // For this I'm going to import LocalDate hihihi...
+    val localMonthNow = LocalDate.now().month // Feeling fancy!
+    val monthNow = localMonthNow.toString().lowercase().capitalize() // Depracated but shorter lulw
+    val birthMonth = monthMap["Aug"] // Used previous map variable
+
+    println("It's $monthNow now, I was born in $birthMonth") // keep it as it is
 
 }
