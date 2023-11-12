eec25189ca69c3a364ac1b5f2f34b8809c421fc7
VML8 <92667459+VML8@users.noreply.github.com>
2023-11-12 18:54:13 +0700

    Exercise 5 Finished...
    This is the most fun out of all exercise, maybe add more flexibility for the scoring dear mentor. I want function jalan() to be able to say where the hero went, and maybe what the hero eat and drink too!

--- src/main/kotlin/KotlinSubmission-Afternoon/exercise5/App.kt
+++ src/main/kotlin/KotlinSubmission-Afternoon/exercise5/App.kt
@@ -8,6 +8,7 @@ fun main() {
      *
      */
 
+    val hero = Hero()
 
     /**
      * Latihan 2
@@ -16,6 +17,7 @@ fun main() {
      *
      */
 
+    hero.setProfile("Luqmannul Hakim", 20, 165)
 
 
     /**
@@ -24,41 +26,12 @@ fun main() {
      *
      */
 
-
+    hero.profile()
 
     /**
     Challenge:
     ===============Class A Afternoon===============
-    4Keys -> jalan 5x, lari 2x, makan 3x, minum 1x, lompat 3x, duduk 1x
-    CELESTIAL -> jalan 2x, lari 2x, makan 1x, minum 3x, lompat 1x, duduk 2x
-    Strix -> jalan 3x, lari 1x, makan 5x, minum 1x, lompat 5x, duduk 3x
-    Fast and Serious -> jalan 5x, lari 4x, makan 4x, minum 4x, lompat 3x, duduk 2x
-    Kuda Terbang -> jalan 4x, lari 1x, makan 3x, minum 5x, lompat 5x, duduk 3x
-    Geek Genius's Team -> jalan 2x, lari 4x, makan 2x, minum 2x, lompat 7x, duduk 4x
-    Enforcers -> jalan 1x, lari 3x, makan 1x, minum 1x, lompat 2x, duduk 1x
-    HA HENG -> jalan 3x, lari 2x, makan 4x, minum 1x, lompat 5x, duduk 1x
-    End Game -> jalan 4x, lari 2x, makan 1x, minum 5x, lompat 2x, duduk 3x
-    Fly Without Wings -> jalan 1x, lari 3x, makan 2x, minum 5x, lompat 4x, duduk 1x
-    Unity Force -> jalan 5x, lari 3x, makan 1x, minum 2x, lompat 3x, duduk 2x
-    11:11 ->7: jalan 1x, lari 2x, makan 3x, minum 4x, lompat 5x, duduk 6x
-
-    ===============Class B Afternoon===============
-    FRINIGHT -> jalan 2x, lari 4x, makan 3x, minum 2x, lompat 1x, duduk 2x
-    EnviGrowth -> jalan 2x, lari 1x, makan 2x, minum 1x, lompat 2x, duduk 4x
-    ZWH (Zero Waste Heroes) -> jalan 1x, lari 2x, makan 3x, minum 4x, lompat 5x, duduk 6x
-    Serenity -> jalan 5x, lari 3x, makan 1x, minum 2x, lompat 3x, duduk 2x
-    UNLIMITED INNOVATION -> jalan 1x, lari 3x, makan 2x, minum 5x, lompat 4x, duduk 1x
-    Procyon -> jalan 4x, lari 2x, makan 1x, minum 5x, lompat 2x, duduk 3x
-    Beta+ -> jalan 3x, lari 2x, makan 4x, minum 1x, lompat 5x, duduk 1x
-    Elevens United -> jalan 1x, lari 3x, makan 1x, minum 1x, lompat 2x, duduk 1x
-    OmniDev -> jalan 2x, lari 4x, makan 2x, minum 2x, lompat 7x, duduk 4x
-    COSMOPOLITE -> jalan 4x, lari 1x, makan 3x, minum 5x, lompat 5x, duduk 3x
-    GrowZen -> jalan 2x, lari 2x, makan 1x, minum 3x, lompat 1x, duduk 2x
-    Unpredictable (Group Merge 12)
-    NusaVote -> jalan 5x, lari 4x, makan 4x, minum 4x, lompat 3x, duduk 2x
-    Vortex -> jalan 2x, lari 1x, makan 2x, minum 1x, lompat 2x, duduk 4x
-
-    **/
+    4Keys -> jalan 5x, lari 2x, makan 3x, minum 1x, lompat 3x, duduk 1x */
 
     /**
      * Latihan 4
@@ -66,7 +39,32 @@ fun main() {
      *
      */
 
+    // Default stamina = 20
+    hero.jalan()    // -1
+    hero.lari()     // -2
+    hero.lompat()   // -2
+    hero.jalan()    // -1
+    hero.jalan()    // -1 = 20-7 = 13
+    // 3 jalan, 1 lari, 1 lompat
 
+    // Istirahat sejenak
+    hero.duduk()    // +1
+    hero.minum()    // +2 = 13+3 = 16
+    // 1 duduk (done), 1 minum (done)
+
+    // Round 2
+    hero.jalan()    // -1
+    hero.lari()     // -2
+    hero.lompat()   // -2
+    hero.makan()    // +2 = 16-3 = 13
+    // 1 jalan (total 4), 1 lari (done), 1 makan, 1 lompat.
+
+    // Remaining action are 1 jalan, 2 makan, 1 lompat
+    // Final Round
+    hero.lompat()   // -2
+    hero.jalan()    // -1
+    hero.makan()    // +2
+    hero.makan()    // +2 = 13+(4-3) = 14
 
     /**
      * Latihan 5
@@ -74,6 +72,7 @@ fun main() {
      *
      */
 
+    hero.profile()
 
 
 }
